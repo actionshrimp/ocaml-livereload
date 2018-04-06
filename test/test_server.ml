731ae1e24bec8f3914fe51f1c50c56e203b0fef6
Dave Aitken <dave.aitken@gmail.com>
2018-04-06 23:15:18 +0100

    Initial skeleton from websocket example

---
+++ test/test_server.ml
@@ -0,0 +1,17 @@
+open Lwt
+open Websocket
+open Websocket_cohttp_lwt
+
+let start_server host port () =
+  let conn_closed (ch,_) =
+    Printf.eprintf "[SERV] connection %s closed\n%!"
+      (Sexplib.Sexp.to_string_hum (Conduit_lwt_unix.sexp_of_flow ch))
+  in
+  Lwt_io.eprintf "[SERV] Listening for HTTP on port %d\n%!" port >>= fun () ->
+  Cohttp_lwt_unix.Server.create
+    ~mode:(`TCP (`Port port))
+    (Cohttp_lwt_unix.Server.make ~callback:Livereload.handler ~conn_closed ())
+
+(* main *)
+let () =
+    Lwt_main.run (start_server "localhost" 7777 ())
