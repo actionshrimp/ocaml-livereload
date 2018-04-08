1bb0c8851daa89e21c60862a1da517478395a9f7
Dave Aitken <dave.aitken@gmail.com>
2018-04-08 14:34:22 +0100

    Working livereload handshake

--- src/livereload.ml
+++ src/livereload.ml
@@ -8,27 +8,36 @@ type msg =
   ; liveCSS: bool
   }
 
-(* let livereload_js = *)
-(*   CCIO.(with_in (Filename.concat "day09.txt") read_lines_l) *)
+let hello =
+{|{
+  "command": "hello",
+  "protocols": ["http://livereload.com/protocols/official-7"],
+  "serverName": "ocaml-livereload"
+}|}
+
+let alert =
+{|{
+  "command": "alert",
+  "message": "HEY!"
+}|}
 
 let handler
     (conn : Conduit_lwt_unix.flow * Cohttp.Connection.t)
     (req  : Cohttp_lwt_unix.Request.t)
-    (body : Cohttp_lwt.Body.t) =
+    (body : Cohttp_lwt.Body.t)
+    (next : Conduit_lwt_unix.flow * Cohttp.Connection.t -> Cohttp_lwt_unix.Request.t -> Cohttp_lwt.Body.t  -> (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t)
+  : ((Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t)
+  =
   let open Frame in
-  Lwt_io.eprintf
-        "[CONN] %s\n%!" (Cohttp.Connection.to_string @@ snd conn)
-  >>= fun _ ->
   let uri = Cohttp.Request.uri req in
-  let _ = Lwt_io.eprintf "[PATH] %s\n%!" (Uri.path uri) in
   match Uri.path uri with
   | "/livereload.js" ->
     Cohttp_lwt_unix.Server.respond_string
     ~status:`OK
     ~body: [%blob "static/livereload.js"]
     ()
-  | "/ws" ->
-    Lwt_io.eprintf "[PATH] /ws\n%!"
+  | "/livereload" ->
+    Lwt_io.eprintf "[livereload] /livereload\n%!"
     >>= fun () ->
     Cohttp_lwt.Body.drain_body body
     >>= fun () ->
@@ -42,31 +51,13 @@ let handler
     )
     >>= fun (resp, body, frames_out_fn) ->
     (* send a message to the client every second *)
-    let _ =
-        let num_ref = ref 10 in
-        let rec go () =
-            if !num_ref > 0 then
-                let msg = Printf.sprintf "-> Ping %d" !num_ref in
-                Lwt_io.eprintf "[SEND] %s\n%!" msg
-                >>= fun () ->
-                Lwt.wrap1 frames_out_fn @@
-                    Some (Frame.create ~content:msg ())
-                >>= fun () ->
-                Lwt.return (num_ref := !num_ref - 1)
-                >>= fun () ->
-                Lwt_unix.sleep 1.
-                >>= go
-            else
-                Lwt_io.eprintf "[INFO] Test done\n%!"
-                >>= Lwt.return
-        in
-        go ()
-    in
-    Lwt.return (resp, (body :> Cohttp_lwt.Body.t))
-  | _ ->
-    Lwt_io.eprintf "[PATH] Catch-all\n%!"
+    Lwt_io.eprintf "[SEND] hello\n%!"
     >>= fun () ->
-    Cohttp_lwt_unix.Server.respond_string
-        ~status:`Not_found
-        ~body:(Sexplib.Sexp.to_string_hum (Cohttp.Request.sexp_of_t req))
-        ()
+    Lwt.wrap1 frames_out_fn @@
+    Some (Frame.create ~content:hello ())
+    >>= fun () ->
+    Lwt.wrap1 frames_out_fn @@
+    Some (Frame.create ~content:alert ())
+    >>= fun () ->
+    Lwt.return (resp, (body :> Cohttp_lwt.Body.t))
+  | _ -> next conn req body
--- test/test_server.ml
+++ test/test_server.ml
@@ -2,6 +2,46 @@ open Lwt
 open Websocket
 open Websocket_cohttp_lwt
 
+let port = 7777
+
+let index = Printf.sprintf {|
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="http://localhost:%d/livereload.js"></script>
+</head>
+<body>
+    <div id='msg'></div>
+</body>
+</html> |} port
+
+let handler
+    (conn : Conduit_lwt_unix.flow * Cohttp.Connection.t)
+    (req  : Cohttp_lwt_unix.Request.t)
+    (body : Cohttp_lwt.Body.t) =
+  Lwt_io.eprintf "[CONN] %s\n%!" (Cohttp.Connection.to_string @@ snd conn)
+  >>= fun _ ->
+  let uri = Cohttp.Request.uri req in
+  let _ = Lwt_io.eprintf "[PATH] %s\n%!" (Uri.path uri) in
+
+  Livereload.handler conn req body
+    (fun conn req body ->
+       let uri = Cohttp.Request.uri req in
+       match Uri.path uri with
+       | "/" ->
+         Cohttp_lwt_unix.Server.respond_string
+           ~status:`OK
+           ~body: index
+           ()
+       | _ ->
+         Lwt_io.eprintf "[PATH] Catch-all\n%!"
+         >>= fun () ->
+         Cohttp_lwt_unix.Server.respond_string
+           ~status:`Not_found
+           ~body:(Sexplib.Sexp.to_string_hum (Cohttp.Request.sexp_of_t req))
+           ()
+    )
+
 let start_server host port () =
   let conn_closed (ch,_) =
     Printf.eprintf "[SERV] connection %s closed\n%!"
@@ -10,8 +50,8 @@ let start_server host port () =
   Lwt_io.eprintf "[SERV] Listening for HTTP on port %d\n%!" port >>= fun () ->
   Cohttp_lwt_unix.Server.create
     ~mode:(`TCP (`Port port))
-    (Cohttp_lwt_unix.Server.make ~callback:Livereload.handler ~conn_closed ())
+    (Cohttp_lwt_unix.Server.make ~callback:handler ~conn_closed ())
 
 (* main *)
 let () =
-    Lwt_main.run (start_server "localhost" 7777 ())
+    Lwt_main.run (start_server "localhost" port ())
