1ead43b2bc7ebff27e19be2317681cade617685a
Matt Bray <mattjbray@gmail.com>
2018-04-18 15:00:20 +0100

    Ensure the watcher doesn't get garbage-collected.

--- src/backend_osx.ml
+++ src/backend_osx.ml
@@ -1,6 +1,7 @@
 open Lwt
 open Types
 
+let watcher_ref = ref None
 
 let make_watcher
     ?(debug = false)
@@ -11,25 +12,19 @@ let make_watcher
 
   let create_flags = Fsevents.CreateFlags.detailed_interactive in
   let watcher = Fsevents_lwt.create 0. create_flags (CCList.map (fun { local_dir } -> local_dir ) path_configs) in
-  let event_stream = Fsevents_lwt.event_stream watcher in
-  let stream = Fsevents_lwt.stream watcher in
+  let () = watcher_ref := Some watcher in (* Don't GC me! *)
   let run_loop_mode = Cf.RunLoop.Mode.Default in
 
-  let cb_f = ref (fun event ->
+  Cf_lwt.RunLoop.run_thread (fun runloop ->
+      Fsevents_lwt.schedule_with_run_loop watcher runloop run_loop_mode;
+      if not (Fsevents_lwt.start watcher)
+      then prerr_endline "Failed to start FSEvents stream"
+    ) >>= fun _rl ->
+
+  Fsevents_lwt.stream watcher
+  |> Lwt_stream.iter_s (fun event ->
       (* TODO: handle path mapping back to server path *)
       let local_path = event.Fsevents_lwt.path in
       debug_log_lwt (Printf.sprintf "[livereload (osx-fsevents)] %s%!" local_path) >>= fun _ ->
       change_cb local_path >>= fun _ ->
       Lwt.return ())
-  in
-
-  Lwt.async (fun () -> Lwt_stream.iter_s !cb_f stream);
-
-  let rt_f = ref (fun runloop ->
-      Fsevents.schedule_with_run_loop event_stream runloop run_loop_mode;
-      if not (Fsevents.start event_stream)
-      then prerr_endline "Failed to start FSEvents stream"
-    )
-  in
-
-  Cf_lwt.RunLoop.run_thread !rt_f; >>= fun _ -> Lwt.return ();
